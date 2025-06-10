ba2e9a5626cd3f3efce9a6d46cc93a68d96b8bfe
zegion <pedromcd999@gmail.com>
2025-06-10 00:18:39 -0400

    -

---
+++ counter-app/src/PrimeraApp.js
@@ -0,0 +1,20 @@
+import React from 'react';
+// import React, { Fragment } from 'react';
+
+
+//Functional components
+const PrimeraApp = () => {
+
+    return (
+        // <Fragment>
+        //     <h1>Hola Mundo</h1>
+        //     <p>Mi componente en react</p>
+        // </Fragment>
+        <>
+            <p>Mi componente en react</p>
+        </>
+    );
+
+}
+
+export default PrimeraApp;
